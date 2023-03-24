fa165eed973eb8e5eb1dfef0b12c5ddfb38ceea4
IDaMariusI <fcovillanueva1988@gmail.com>
2023-03-24 10:28:11 -0600

    164. Monto y comprar ahora

--- lib/src/pages/shoe_desc_page.dart
+++ lib/src/pages/shoe_desc_page.dart
@@ -7,24 +7,47 @@ class ShoeDescPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
-          ShoeSizePreview(fullScreen: true),
+          const ShoeSizePreview(fullScreen: true),
           Expanded(
             child: SingleChildScrollView(
               child: Column(
                 children: [
-                  ShoeDescription(
+                  const ShoeDescription(
                     title: 'Nike Air Max 720',
                     description:
                         "The Nike Air Max 720 goes bigger than ever before with Nike's taller Air unit yet, offering more air underfoot for unimaginable, all-day comfort. Has Air Max gone too far? We hope so.",
                   ),
+                  _PriceBuyNow(),
                 ],
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _PriceBuyNow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Container(
+        margin: const EdgeInsets.only(top: 20),
+        child: const Row(
+          children: [
+            Text(
+              '\$180.0',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+            ),
+            Spacer(),
+            OrangeButton(text: 'Buy now', height: 40.0, width: 120.0),
+          ],
+        ),
       ),
     );
   }
--- lib/src/widgets/orange_button.dart
+++ lib/src/widgets/orange_button.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 
 class OrangeButton extends StatelessWidget {
-  const OrangeButton({super.key, required this.text});
+  const OrangeButton({
+    super.key,
+    this.height = 50.0,
+    this.width = 150.0,
+    required this.text,
+  });
 
+  final double height;
+  final double width;
   final String text;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      height: 50,
-      width: 150,
+      height: height,
+      width: width,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(100),
         color: Colors.orange,
